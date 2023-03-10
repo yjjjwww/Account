9aa600b296e9c30f72ddac2d6fd04ab2ab72dad3
yjjjwww <yjjjwww@naver.com>
2023-03-10 22:33:01 +0900

    add : AOP lock service

--- src/main/java/com/example/account/controller/AccountController.java
+++ src/main/java/com/example/account/controller/AccountController.java
@@ -1,12 +1,10 @@
 package com.example.account.controller;
 
 import com.example.account.domain.Account;
-import com.example.account.dto.AccountDto;
 import com.example.account.dto.AccountInfo;
 import com.example.account.dto.CreateAccount;
 import com.example.account.dto.DeleteAccount;
 import com.example.account.service.AccountService;
-import com.example.account.service.RedisTestService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,7 +16,6 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class AccountController {
     private final AccountService accountService;
-    private final RedisTestService redisTestService;
 
     @PostMapping("/account")
     public CreateAccount.Response createAccount(
@@ -30,11 +27,6 @@ public class AccountController {
                         request.getInitialBalance()
                 )
         );
-    }
-
-    @GetMapping("/get-lock")
-    public String getLock() {
-        return redisTestService.getLock();
     }
 
     @GetMapping("/account/{id}")
